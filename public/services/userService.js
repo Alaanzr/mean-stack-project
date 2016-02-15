5a907ecfd2ae953b4e7e1a58486f91b63a6353c5
Alaanzr <Alaanzr@gmail.com>
2016-02-15 02:18:09 +0000

    Minor CSS changes

--- public/services/userService.js
+++ public/services/userService.js
@@ -1,5 +1,5 @@
 meanApp.factory('Users', ['$resource', function($resource) {
-  return $resource('/users', {}, {
+  return $resource('/users', {
     query: {
       method: 'GET',
       isArray: true
